e1f61f6aa7f0fc90457b15edd40335632b2a238c
CritianF05 <1482670@senati.pe>
2023-03-04 09:39:28 -0500

    lista imprimir el primer dato y el ultimo

---
+++ lista_trabajo.py
@@ -0,0 +1,9 @@
+#la lista
+l = ["red", "blue", "hello", "white"]
+#para imprimir la lista
+print(l)
+#para imprimir el primir dato
+print("el primer dato es ", l [0])
+#para imprimir el ultimo dato
+print("el ultimo dato es", l [3])
+
